4c215c482cbd82e8b5e6529760eda7f40b0e3488
bolts9566 <bolts9566@SHS2512-Z400-08.pkwy.k12.mo.us>
2013-01-10 12:44:58 -0600

    Added methods for player class and player arrays for pieces

--- Trouble/src/TroubleProject/Player.java
+++ Trouble/src/TroubleProject/Player.java
@@ -1,23 +1,48 @@
 package TroubleProject;
 
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 
 public class Player {
 
 
 	
-	Image[] playerImgs = new Image[4];
-	int[] playerX;
-	int[] playerY;
+	private Image[] playerImgs;
+	private int[] pos;
+	private boolean[] playerInStart, pieceScored;
 	
-	public Player(String imgRef) throws SlickException{
-	
-		for(int i = 0; i < playerImgs.length; i ++){
+	public Player(String imgRef, int team) throws SlickException{
+		
+		pos = new int[4];
+		pieceScored = new boolean[4];
+		playerInStart = new boolean[4];
+		playerImgs = new Image[4];
+		
+		for(int i = 0; i < 4; i ++){
 		playerImgs[i] = new Image(imgRef);
-		}
-		playerX = new int[4];
-		playerY = new int[4];
+		playerInStart[i] = true;
+		pieceScored[i] = false;
+		}	
 	}
+	
+public void setPos(int piece, int position){
+	pos[piece] = position;
+}
 
+public void started(int piece){
+	playerInStart[piece] = false;
+}
+
+public void scored(int piece){
+	pieceScored[piece] = true;
+}
+
+public void drawPlayer(Rectangle[] rect, Graphics g, int playerPiece){
+
+	g.drawImage(playerImgs[playerPiece], rect[playerPiece].getX(), rect[playerPiece].getY());
+	
+}
+	
 }
